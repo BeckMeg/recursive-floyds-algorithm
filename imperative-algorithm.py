2c17a4b3f4834b8b7dfca56c541d43eac88bcef5
BeckMeg <sgmbeck@liverpool.ac.uk>
2022-05-04 21:38:45 +0100

    imperative version of floyd-warshall algorithm

--- imperative-algorithm.py
+++ imperative-algorithm.py
@@ -1,18 +1,37 @@
-def floyd(distance):
-"""
-A simple implementation of Floyd's algorithm
-"""
-for intermediate, start_node,end_node\
-in itertools.product\
-(range(MAX_LENGTH),range(MAX_LENGTH), range(MAX_LENGTH)):
-# Assume that if start_node and end_node are the same
-# then the distance would be zero
-if start_node == end_node:
-distance[start_node][end_node] = 0
-continue
-#return all possible paths and find the minimum
-distance[start_node][end_node] = min(distance[start_node][end_node],
-distance[start_node][intermediate] + distance[intermediate][end_node] )
-#Any value that have sys.maxsize has no path
-print (distance)
-floyd(graph)
+# Number of vertices
+V = 4
+INF = 999
+
+
+# Algorithm 
+def floyd(g):
+    dist = list(map(lambda i: list(map(lambda j: j, i)), g))
+
+    # Adding vertices individually
+    for k in range(V):
+        for i in range(V):
+            for j in range(V):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+    sol(dist)
+
+
+# Printing the output
+def sol(dist):
+
+    for i in range(V):
+        for j in range(V):
+            if dist[i][j] == INF:
+                print("INF", end=" ")
+            else:
+                print(dist[i][j], end="  ")
+
+        print(" ")
+
+
+G = [[0, 5, INF, 10],
+     [INF, 0, 3, INF],
+     [INF, INF, 0, 1],
+     [INF, INF, INF, 0]]
+floyd(G)
+
