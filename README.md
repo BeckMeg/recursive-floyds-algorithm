b067f4c948994bc9900fb9e5ab5b10ba5ef41ac8
BeckMeg <sgmbeck@liverpool.ac.uk>
2022-05-06 10:29:42 +0100

    PEP standards added

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Recursive Floyds Algorithm
+Floyd's algorithm finds the shortest path between all pairs
+of vertices in a graph. 
 
-Rewrite of Floyd's Algorithm to use recursion
+Floyd's Algorithm has been rewritten to use recursion to complete 
+a mid-module assignment for the Univerity of Liverpool.
+PyCharm Community was used to develop this algorithm. 
+
+Find a copy of the GeeksforGeeks interative algorithm
+in imperative-algorithm.py
+
+The recursive version of this algorithm can be found in the 
+recursive-floyd.py file
+
+## Testing 
+
+The output of the tests are found in th
--- recursive-floyd.py
+++ recursive-floyd.py
@@ -2,22 +2,27 @@
 
 def floyd(shortestPath):
 
+    # This defines the vertices input of the graph
     V = len(shortestPath)
 
     def recursive_floyd(i, j, k):
 
-        # This is the base case
+        # This is the base case.
+        # When there are no intermediary vertices in the path.
         if k == -1:
             return shortestPath((i, j))
 
+        # When using k to calculate the shortestPath, the minimum distance is returned
         else:
             return min(recursive_floyd(i, j, k-1),
                        recursive_floyd(i, k, k-1)
                        + recursive_floyd(j, k, k-1))
-    sol(shortestPath)
 
+    # The values used to find the shortestPath and keep it updated
     for k in range(V):
         for i in range(V):
             for j in range(V):
                 shortestPath[i][j] = recursive_floyd(i, j, k-1)
+    # Return the shortest path
+    """This is the shortest path found using recursive_floyd"""
     return shortestPath
